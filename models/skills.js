a5ecd8af19d680ab1d80cc1a9b84a58ecf7328f9
chris-gallegos <chrisgallegos101@gmail.com>
2022-09-27 14:46:41 -0700

    added models and controllers folders

---
+++ models/skills.js
@@ -0,0 +1,14 @@
+const skills = [
+    {id: 125223, skill: 'Dancing', done: true},
+    {id: 127904, skill: 'Singing', done: false},
+    {id: 139608, skill: 'Cooking', done: false}
+  ];
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
